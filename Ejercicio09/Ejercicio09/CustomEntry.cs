939263ecb7de8d816cf00439fae0eee58b40dfa2
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-21 23:20:27 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Ejercicio09/CustomEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Ejercicio09
+{
+    public class CustomEntry :Entry
+    {
+    }
+}
---
+++ Ejercicio09/Ejercicio09/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio09
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            tareacss.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new tareacss());
+            };
+            ExplicitC.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            Explicit.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            ImplicitC.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+            Implicit.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+            Global.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new globaldemo());
+            };
+            Inherente.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
+            Dynamic.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+            };
+        }
+    }
+}
